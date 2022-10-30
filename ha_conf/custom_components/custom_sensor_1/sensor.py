faf0e8201b1e0eddbb8b4e9c40c3f85d1ff9f811
Markus <m4k.join@outlook.com>
2022-10-30 23:23:26 +0100

    fix python debug launch

--- ha_conf/custom_components/custom_sensor_1/sensor.py
+++ ha_conf/custom_components/custom_sensor_1/sensor.py
@@ -14,33 +14,26 @@ HANDLED:
 """
 
 
-from custom_components.custom_sensor_1.my_api.client import (
-    MyApiClient, DummyClass)
-from homeassistant.components.sensor import (
-    SensorDeviceClass,
-    SensorEntity,
-    SensorStateClass,
-)
-from homeassistant.helpers.update_coordinator import (
-    CoordinatorEntity,
-    DataUpdateCoordinator,
-    UpdateFailed,
-)
-from homeassistant.helpers.entity_platform import AddEntitiesCallback
-from homeassistant.exceptions import (ConfigEntryAuthFailed, PlatformNotReady)
-from homeassistant.core import callback
-from homeassistant.core import HomeAssistant
-from homeassistant.config_entries import ConfigEntry
-from homeassistant.const import TEMP_CELSIUS
-
-from enum import Enum
-
-import async_timeout
-from datetime import timedelta
-
+from typing import TypedDict
+from dataclasses import replace
 import logging
-
-DOMAIN = "custom_sensor_1"
+from datetime import timedelta
+from tokenize import String
+import async_timeout
+from enum import Enum
+from homeassistant.const import TEMP_CELSIUS
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
+from homeassistant.core import callback
+from homeassistant.exceptions import (ConfigEntryAuthFailed, PlatformNotReady)
+from homeassistant.helpers.entity_platform import AddEntitiesCallback
+from homeassistant.helpers.template import device_entities
+from homeassistant.helpers.template import integration_entities
+from homeassistant.helpers.entity_registry import async_get
+from custom_components.custom_sensor_1.my_api.client import (MyApiClient, DummyClass)
+from homeassistant.components.sensor import (SensorDeviceClass, SensorEntity, SensorStateClass)
+from homeassistant.helpers.update_coordinator import (CoordinatorEntity, DataUpdateCoordinator, UpdateFailed)
+_DOMAIN_ = "custom_sensor_1"
 
 _LOGGER = logging.getLogger(__name__)
 _LOGGER.debug("Starting %s", __file__)
@@ -53,6 +46,12 @@ _managed_entity_ids: list[str] = []
 # TODO: Find the issue why only dd is updated after a few tries (ee is LOST)
 # Hint: maybe the for loop or the coord.data return
 # Hint: be aware the dd is the last added entry
+
+# def log_debug_all_items(__coordinator_data: dict[DummyClass]):
+#     _LOGGER.debug("Log all coo data")
+#     for key,value in __coordinator_data.items():
+#         _LOGGER.debug(__coordinator_data[key].at_id)
+#         _LOGGER.debug(__coordinator_data[key].extra_attributes)
 
 
 async def async_setup_platform(hass: HomeAssistant, config, async_add_entities, discovery_info=None) -> None:
@@ -72,10 +71,66 @@ async def async_setup_platform(hass: HomeAssistant, config, async_add_entities, 
         raise PlatformNotReady
 
     async_add_entities(CoordinatedExampleSensor(
-        _coordinator=coordinator, _unique_id=_uid) for _uid in _managed_entity_ids)
+        _coord=coordinator, _unique_id=_uid) for _uid in _managed_entity_ids)
 
     _LOGGER.debug("current coordinator data:")
     _LOGGER.debug(coordinator.data)
+
+    _LOGGER.debug("hass-data")
+    _LOGGER.debug(hass.data['integrations'][_DOMAIN_])
+
+    log_entities_all(hass)
+    # error: _LOGGER.debug(hass.data['integrations'][_DOMAIN_][_DOMAIN_])
+
+    # _LOGGER.debug(hass.data)
+
+    # raise "halt and catch fire"
+    # hass.stop()
+    # log_debug_all_items(coordinator.data)
+
+
+def log_domains(_hass: HomeAssistant):
+    _LOGGER.debug("ENTER: log_domains")
+    _LOGGER.debug("DOMAIN LOGGIN DISABLED")
+    # _LOGGER.debug("Start Domains:")
+    # do = _hass.states.async_all()
+    # _LOGGER.debug(do.domain)
+    # _LOGGER.debug("Done Domains")
+    # _LOGGER.debug("Start Domains2:")
+    # for entity_id in _hass.states.entity_ids():
+    #     count_all = count_all + 1
+    #     entity_domain = entity_id.split('.')[0]
+    #     if entity_domain not in domains:
+    #         domains.append(entity_domain)
+    # _LOGGER.debug(domains)
+    # _LOGGER.debug("Done Domains2:")
+    _LOGGER.debug("LEAVE: log_domains")
+
+
+def log_entity_ids(_hass: HomeAssistant):
+    _LOGGER.debug("ENTER: log_entity_ids")
+    re = _hass.states.async_entity_ids()
+    _LOGGER.debug("print all:")
+    for r in re:
+        _LOGGER.debug(r)
+    _LOGGER.debug("LEAVE: log_entity_ids")
+    # ent = integration_entities(hass,_DOMAIN_)
+    # _LOGGER.debug("entieees")
+    # _LOGGER.debug(ent)
+
+
+def log_entities_all(_hass: HomeAssistant):
+    _LOGGER.debug("ENTER: log_entities_all")
+    _LOGGER.debug("get all states/sensors")
+    re = _hass.states.async_all()
+    _LOGGER.debug("show how many:")
+    _LOGGER.debug(len(re))
+    _LOGGER.debug("print all:")
+    for r in re:
+        _LOGGER.debug(r)
+    _LOGGER.debug("LEAVE: log_entities_all")
+    log_entity_ids(_hass)
+    log_domains(_hass)
 
 
 class MyCoordinator(DataUpdateCoordinator):
@@ -91,15 +146,18 @@ class MyCoordinator(DataUpdateCoordinator):
             hass,
             _LOGGER,
             # Name of the data. For logging purposes.
-            name=DOMAIN,
+            name=_DOMAIN_,
+            update_method=self._async_update_data,
             # Polling interval. Will only be polled if there are subscribers.
             # default is usually 30 seconds
             update_interval=timedelta(seconds=40),
         )
         self._my_api = my_api
         _LOGGER.debug("managed entity ids: %s", _managed_entity_ids)
+        log_entities_all(hass)
 
-    async def _async_update_data(self) -> dict[DummyClass]:
+    # TODO: Replace dict[DummyClass] -> in this case, a list of dummyclass is stored - but it doesnt need to be a dict
+    async def _async_update_data(self) -> dict:
         """Fetch data from API endpoint.
         This is the place to pre-process the data to lookup tables
         so entities can quickly look up their data.
@@ -113,7 +171,7 @@ class MyCoordinator(DataUpdateCoordinator):
                 _LOGGER.debug("start self.my_api.get_dummy_data()")
                 # call api to get the data object
                 # put the dataobject into the HA-store
-                coordinator_data: dict[DummyClass] = {}
+                coordinator_data: dict = {}
                 for __uid in _managed_entity_ids:
                     _LOGGER.debug("calling api for id: %s", __uid)
                     # here you could also do an if id = "abc" then talk to api-a else api-b
@@ -121,6 +179,8 @@ class MyCoordinator(DataUpdateCoordinator):
                     coordinator_data[__uid] = dummy_data
                 _LOGGER.debug("returning cordinator_data:")
                 _LOGGER.debug(coordinator_data)
+                # log_debug_all_items(coordinator_data)
+                log_entities_all(self.hass)
                 return coordinator_data
 
         except ConfigEntryAuthFailed as err:
@@ -131,6 +191,11 @@ class MyCoordinator(DataUpdateCoordinator):
             raise UpdateFailed(f"Error communicating with API: {err}")
 
 
+class extra_coord_sens(TypedDict):
+    system_state: str
+    idx: str
+
+
 class CoordinatedExampleSensor(CoordinatorEntity, SensorEntity):
     """An entity using CoordinatorEntity.
     The CoordinatorEntity class provides:
@@ -139,7 +204,7 @@ class CoordinatedExampleSensor(CoordinatorEntity, SensorEntity):
         async_added_to_hass
         available
     """
-    __coordinator: CoordinatorEntity
+    _coord: MyCoordinator
 
     _LOGGER.debug("""Class of CoordinatedExampleSensor""")
 
@@ -148,15 +213,9 @@ class CoordinatedExampleSensor(CoordinatorEntity, SensorEntity):
     _attr_device_class = SensorDeviceClass.TEMPERATURE
     _attr_state_class = SensorStateClass.MEASUREMENT
 
-    # declare keys for easier use
-    _attr_system_state = "system_state"
-    _attr_id = "id"
     # use the keys and assign an empty value
     # its content will be visible in HA
-    extra_attributes: dict = {
-        _attr_system_state: "",
-        _attr_id: ""
-    }
+    extra_attrib: extra_coord_sens = {"idx": "", "system_state": ""}
 
     def get_sensor_value(self) -> int:
         """Retrieve the current sensor value from the coordinator
@@ -165,64 +224,89 @@ class CoordinatedExampleSensor(CoordinatorEntity, SensorEntity):
         """
         _LOGGER.debug("_get_data of CoordinatedExampleSensor")
         _LOGGER.debug("getting sensor value for id: %s",
-                      self.extra_attributes[self._attr_id])
-        entity_id = self.extra_attributes[self._attr_id]
-        __dummy_data: DummyClass = self.__coordinator.data[entity_id]
+                      self.extra_attrib["idx"])
+        entity_id = self.extra_attrib["idx"]
+        __dummy_data: DummyClass = self._coord.data[entity_id]
         value = __dummy_data.dummyvalue
-        value = self.__coordinator.data[self.extra_attributes[self._attr_id]].dummyvalue
-        _LOGGER.debug("current  self.__coordinator data:")
-        _LOGGER.debug(self.__coordinator)
+        value = self._coord.data[self.extra_attrib["idx"]].dummyvalue
+        _LOGGER.debug("current self.__coordinator.data:")
+        _LOGGER.debug(self._coord.data)
         _LOGGER.debug("returning %i", value)
         return value
 
     def get_sensor_system_state(self) -> str:
         _LOGGER.debug("_get_sensor_system_state of CoordinatedExampleSensor")
         _LOGGER.debug("getting sensor system state for id: %s",
-                      self.extra_attributes[self._attr_id])
+                      self.extra_attrib["idx"])
         # value = "fixed-offline"
-        value = self.__coordinator.data[self.extra_attributes[self._attr_id]].dummystate
+        value = self._coord.data[self.extra_attrib["idx"]].dummystate
         _LOGGER.debug("returning %s", value)
         return value
 
     def update_all_data(self) -> None:
         # not rquired, but recommended updates all properties and values of the entity(sensor)
         _LOGGER.debug("update_all_data of CoordinatedExampleSensor")
-        _LOGGER.debug("current  self.__coordinator data:")
-        _LOGGER.debug(self.__coordinator)
+        _LOGGER.debug("enter for id: %s", self.extra_attrib["idx"])
+        _LOGGER.debug("current self.__coordinator.data:")
+        _LOGGER.debug(self._coord.data)
         self._attr_native_value = self.get_sensor_value()
-        self.extra_attributes[self._attr_system_state] = self.get_sensor_system_state(
-        )
+        self.extra_attrib["idx"] = self.get_sensor_system_state()
+        # not working: log_entities(self.hass)
 
     @property
-    def extra_state_attributes(self) -> dict:
+    def extra_state_attributes(self) -> extra_coord_sens:
         """Return entity specific state attributes."""
+        _LOGGER.debug("enter for id: %s", self.extra_attrib["idx"])
         # required to store your own attributes, like _attr_testprop
         # should only be done the state is of the own attributes are not changed frequently
         # else you should use another sensor
         _LOGGER.debug("extra_state_attributes of CoordinatedExampleSensor")
-        return self.extra_attributes
+        return self.extra_attrib
 
     @callback
     def _handle_coordinator_update(self) -> None:
         """Handle updated data from the coordinator."""
         _LOGGER.debug("_handle_coordinator_update of CoordinatedExampleSensor")
+        _LOGGER.debug("enter for id: %s", self.extra_attrib["idx"])
         # gets triggered, triggers the pull of the data from the coordinator
         # gets triggered after the coordinator has finished getting updates
-        _LOGGER.debug("current  self.__coordinator data:")
-        _LOGGER.debug(self.__coordinator)
+        _LOGGER.debug("current self.__coordinator data:")
+        _LOGGER.debug(self._coord.data)
         self.update_all_data()
         # self.async_write_ha_state()
+        log_entities_all(self.hass)
         return super()._handle_coordinator_update()
 
-    def __init__(self, _coordinator: CoordinatorEntity, _unique_id: str):
+    def __init__(self, _coord: MyCoordinator, _unique_id: str) -> None:
         """Pass coordinator to CoordinatorEntity."""
-        _LOGGER.debug("__init__ of CoordinatedExampleSensor")
+        _LOGGER.debug("__init__ of CoordinatedExampleSensor %s", _unique_id)
         _LOGGER.debug("current _coordinator data:")
-        _LOGGER.debug(_coordinator)
-        super().__init__(_coordinator)
-        self.__coordinator = _coordinator
-        _LOGGER.debug("current  self.__coordinator data:")
-        _LOGGER.debug(self.__coordinator)
-        self._attr_name = self._attr_name.replace("Dummy", _unique_id)
-        self.extra_attributes[self._attr_id] = _unique_id
+        _LOGGER.debug(_coord.data)
+        # log_debug_all_items(_coordinator.data)
+        self._coord = _coord
+        _LOGGER.debug("current self.__coordinator data:")
+        _LOGGER.debug(self._coord.data)
+        # log_debug_all_items(self.__coordinator.data)
+        if (self._attr_name is not None):
+            dum: str = self._attr_name
+        else:
+            dum = ""
+        self._attr_name = dum.replace("Dummy", _unique_id)
+        # self.extra_attrib["idx"] = _unique_id
+        # AttributeError: 'CoordinatedExampleSensor' object has no attribute 'extra_attrib'
+        # TODO: solve issue where object not existing (needs to be created / new)
+
+        #self.extra_attrib = extra_coord_sens()
+
+        self.extra_attrib["idx"] = _unique_id
         self.update_all_data()
+        super().__init__(_coord)
+        # log_debug_all_items(_coordinator.data)
+        #raise "hier"
+        # TODO: Find issue why the second attribute gets overwriten
+        #!check the attribute_name / and attribute id of both objects
+
+        # in coordinator.data sind dummyclass objekte und nicht CoordinatedExampleSensor Objekte
+        # TODO: For debug purpose write functions which finds and logs all info
+        # of CoordinatedExampleSensor Objects in HA entities store
+        # not working, producing error log_entities(self.hass)
