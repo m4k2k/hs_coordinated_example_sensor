df184cb12ef335ab863876e207b86d866b519bbf
Markus <m4k.join@outlook.com>
2022-10-30 23:24:35 +0100

    exten local run script

--- run_dev_ha.ps1
+++ run_dev_ha.ps1
@@ -1,11 +1,22 @@
 
+docker stop devha
+
 $conf_folder = 'C:\Dev\Proj\ha_dev\ha_conf'
 
-
 $log_filepath = "$conf_folder\home-assistant.log"
+$old_log_folder = "old_logs"
 $log_file = Get-Item -Path $log_filepath
-Rename-Item -Path $log_file.FullName -NewName "$($log_file.BaseName)_$(get-date -f yyyy-MM-dd_H-mm_s)$($log_file.Extension)"
+$new_log_file = "$($log_file.BaseName)_$(get-date -f yyyy-MM-dd_H-mm_s)$($log_file.Extension)"
+Rename-Item -Path $log_file.FullName -NewName $new_log_file
 New-Item -Path $log_file.Directory -Name $log_file.Name -ItemType File
+New-Item -Path "$conf_folder\" -Name $old_log_folder -ItemType Directory
+Move-Item -Path "$conf_folder\$new_log_file" -Destination "$conf_folder\$old_log_folder\"
+
+$conf_entries_file = "$conf_folder\.storage\core.config_entries"
+$conf_registry_file = "$conf_folder\.storage\core.entity_registry"
+
+Remove-Item -Path $conf_entries_file
+Remove-Item -Path $conf_registry_file
 
 docker run -it --rm `
     -e "TZ=Europe/Berlin" `
