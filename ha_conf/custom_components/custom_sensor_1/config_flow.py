1f71dad7fca83164ac8fd1f4aa39aefe1c8d4399
Markus <m4k.join@outlook.com>
2022-11-14 23:06:06 +0100

    add config_flow

--- ha_conf/custom_components/custom_sensor_1/config_flow.py
+++ ha_conf/custom_components/custom_sensor_1/config_flow.py
@@ -3,13 +3,10 @@ from typing import Any, Mapping
 
 from homeassistant import config_entries, data_entry_flow
 
-# from .const import DOMAIN
-
 _DOMAIN_ = "custom_sensor_1"
 
 _LOGGER = logging.getLogger(__name__)
-
-_LOGGER.debug("Enter: %s", __file__)
+_LOGGER.debug("START %s", __file__)
 
 
 @config_entries.HANDLERS.register(_DOMAIN_)
@@ -19,15 +16,20 @@ class TestConfigFlow(config_entries.ConfigFlow):
     # Home Assistant will call your migrate method if the version changes
     # (this is not implemented yet)
     VERSION = 1
-    _LOGGER.debug("Enter: TestConfigFlow of %s", __file__)
+    _LOGGER.debug("ENTER TestConfigFlow of %s", __file__)
 
     async def async_step_user(
         self, user_input: dict[str, Any] | None = None
     ) -> data_entry_flow.FlowResult:
-        """Handle a flow initiated by the user."""
-        _LOGGER.debug("Enter: async_step_user")
+        """Handle a flow, without user intaction """
+        _LOGGER.debug("ENTER async_step_user of TestConfigFlow in %s", __file__)
+        # Set unique ID
         await self.async_set_unique_id("testdev01")
+        # If unique ID is already present, abort and exist config setup,
+        # message of /translations/en.json is shown (no error)
         self._abort_if_unique_id_configured()
+        # create empty config entry
         retval: Mapping[str, Any] = {}
         _LOGGER.debug("Create Config Entry")
+        # config entry is found in ha_conf\.storage\core.config_entries
         return self.async_create_entry(title="test", data=retval)
