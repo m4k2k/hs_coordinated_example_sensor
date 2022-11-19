8492118d2d17a34e97b4d064bc4312dcd6eda34a
Markus <m4k.join@outlook.com>
2022-11-19 20:37:21 +0100

     

--- run_dev_ha.ps1
+++ run_dev_ha.ps1
@@ -1,7 +1,7 @@
 
 docker stop devha
 
-$conf_folder = 'C:\Dev\Proj\ha_dev\ha_conf'
+$conf_folder = "$PSScriptRoot\ha_conf"
 
 $log_filepath = "$conf_folder\home-assistant.log"
 $old_log_folder = "old_logs"
@@ -12,6 +12,7 @@ New-Item -Path $log_file.Directory -Name $log_file.Name -ItemType File
 New-Item -Path "$conf_folder\" -Name $old_log_folder -ItemType Directory -ErrorAction Ignore
 Move-Item -Path "$conf_folder\$new_log_file" -Destination "$conf_folder\$old_log_folder\"
 
+# remove db folders (delete devices and entitites)
 $files_to_clear = @('core.device_registry', 'core.config_entries', 'core.entity_registry')
 foreach ($_file in $files_to_clear) {
     Remove-Item -Path "$conf_folder\.storage\$_file" -Force -ErrorAction Continue
@@ -25,10 +26,8 @@ docker run -it --rm `
     --mount type=bind,source="$conf_folder",target="/config" `
     homeassistant/home-assistant
 
+# Local address of homeassistant
 # http://localhost:8123/
 
-# usr: tempdevinstall
-# pwd: §g23/(egt3253EHF
-
-
+# connect to python inside docker (exposing on port 6712)
 # python -m debugpy --connect 127.0.0.1:6712
